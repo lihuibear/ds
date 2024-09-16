dc7954c1112e0a7fa68e92986e6b5d8f677a0f62
lihuibear <lihuibear4@gmail.com>
2024-09-16 17:02:04 +0800

    p2

---
+++ p2/p2_1cpp.cpp
@@ -0,0 +1,24 @@
+// 算法初体验
+// 1到100的和
+
+#include <stdio.h>
+// 累加
+
+//int main(){
+//int i = 0;
+//int sum = 0;
+//int n = 100;
+//for(i=1;i <=n;i++){
+//sum = sum + i;
+//}
+//printf("%d",sum);
+//}
+
+int main(){
+	int i = 0;
+	int sum = 0;
+	int n = 100;
+	sum = (i+1+n)*n/2;
+	printf("%d",sum);
+}
+
